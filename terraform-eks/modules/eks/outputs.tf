1cdac29ad8253b27f257eb0b412392377f55afa8
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-13 22:38:17 +0500

    Update outputs.tf

--- terraform-eks/modules/eks/outputs.tf
+++ terraform-eks/modules/eks/outputs.tf
@@ -1,13 +1,36 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "kubeconfig" {
+  description = "Command to configure kubectl"
   value = <<EOT
 aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.this.name}
 EOT
+}
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "node_group_name" {
+  description = "Name of the EKS node group"
+  value       = aws_eks_node_group.private.node_group_name
 }

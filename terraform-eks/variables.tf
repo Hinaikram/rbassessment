12bc162da4d9081d4a6d73e04c42475b5632460a
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-13 22:34:46 +0500

    Update variables.tf

--- terraform-eks/variables.tf
+++ terraform-eks/variables.tf
@@ -1,37 +1,36 @@
 variable "region" {
-  description = "AWS region"
-  default     = "ap-south-1"  # Or use your region
+  description = "AWS region to deploy the infrastructure in"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "EKS Cluster name"
-  default     = "my-cluster"  # Replace with your desired cluster name
+  description = "Name of the EKS cluster"
+  default     = "my-cluster"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs"
+  description = "List of CIDRs for public subnets (used for NAT Gateways or Internet-facing services)"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of private subnet CIDRs"
+  description = "List of CIDRs for private subnets (used for worker nodes)"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "tf_state_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
-  default     = "rb-state-bucket"  # Make sure this is unique
+  description = "Name of the S3 bucket to store Terraform remote state"
+  default     = "rb-state-bucket"  # Ensure this name is globally unique
 }
 
 variable "tf_lock_table_name" {
-  description = "Name of the DynamoDB table for state locking"
+  description = "Name of the DynamoDB table used for Terraform state locking"
   default     = "terraform-locks"
 }
-

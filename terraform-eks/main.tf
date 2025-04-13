31818aa7f18cd11e4355a3f021882e7ce9b2fca8
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-13 23:19:35 +0500

    Update main.tf

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -66,39 +66,3 @@ resource "aws_security_group" "sg" {
   }
 }
 
-# Optional: Define S3 bucket for storing Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.tf_state_bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-# Optional: Define DynamoDB table for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.tf_lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}

e5e14e763e779210a1170e95ddcf77bb7861bb9a
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-14 00:17:37 +0500

    Update outputs.tf

--- terraform-eks/outputs.tf
+++ terraform-eks/outputs.tf
@@ -9,3 +9,12 @@ output "cluster_name" {
 output "kubeconfig" {
   value = module.eks.kubeconfig
 }
+# Output for public subnet IDs
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+# Output for private subnet IDs
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}

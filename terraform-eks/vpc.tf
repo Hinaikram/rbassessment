b18cb22f51cff56ed452eb2a5a363cdc321ef9d3
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-14 04:46:46 +0500

    Update vpc.tf

--- terraform-eks/vpc.tf
+++ terraform-eks/vpc.tf
@@ -1,27 +1,37 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
-}
 
-  name = "vpc"
-
+  # VPC Configuration
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  
+  azs  = data.aws_availability_zones.available.names # Automatically selects the availability zones
 
+  # Subnet Configuration
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
+  # NAT Gateway Configuration
   enable_nat_gateway   = true
-  single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  public_subnet_tags = {
+  # Tags for VPC and Subnets
+  tags = {
+    "Name" = "vpc"
+  }
+
+  subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = 1
   }
 
+  # If you need more custom subnet tags, you can set them directly
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = 1
   }
+
+  # Additional options based on your requirement
+  create_nat_gateway = true
+  create_vpc          = true
 }

9aba37379a4600fbb860fd1a70c1024108e0a563
Hinaikram <156688171+Hinaikram@users.noreply.github.com>
2025-04-14 07:47:44 +0500

    Update eks-cluster.tf

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -3,11 +3,20 @@ module "eks" {
   version = "19.13.1"
 
   cluster_name    = "rb-cluster"
-  cluster_version = "1.26"  # Updated Kubernetes version to a supported one
+  cluster_version = "1.26"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   cluster_endpoint_public_access = true
+
+  create_cloudwatch_log_group = false  # <-- Avoid conflict with existing log group
+
+  # Optional: Use an existing KMS key if encryption is enabled
+  # If you're not using encryption, you can omit the below block
+  # cluster_encryption_config = {
+  #   resources        = ["secrets"]
+  #   provider_key_arn = "arn:aws:kms:ap-south-1:<your-account-id>:key/<existing-kms-key-id>"
+  # }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
@@ -20,8 +29,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 1
+      desired_size = 1
     }
 
     two = {
@@ -30,9 +39,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
     }
   }
 }
-
